eedfb20e8dc5042a918a006957aa3f9a5f172817
LVargas <luish.vargas09@gmail.com>
2026-09-12 18:34:32 -0600

    feat: Dashboard útil y más profesional

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 const supabaseUrl = "https://uazlzqlxlcydeygpmreo.supabase.co";
-const supabaseKey = "sb_publishable_NKESrfYA2Shu3_Xw4xsZ8g_vAAiIGiv"; // No uses service_role
+const supabaseKey = "sb_publishable_NKESrfYA2Shu3_Xw4xsZ8g_vAAiIGiveyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhemx6cWx4bGN5ZGV5Z3BtcmVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkxNDk5MDIsImV4cCI6MjEwNDcyNTkwMn0.IBWmwkBRmDj_pl8ByEb6tEN2-aEQOEyncjqLf_UurXk"; // No uses service_role
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
